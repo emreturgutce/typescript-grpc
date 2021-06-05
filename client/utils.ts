85c19b29702626796a395947e4833376edba138b
Emre Turgut <emrebursalee@gmail.com>
2021-06-05 20:48:55 +0300

    users

---
+++ client/utils.ts
@@ -0,0 +1,11 @@
+import { credentials } from 'grpc';
+import { UsersClient } from '../proto/users_grpc_pb';
+
+const port = 3000;
+
+export const client = new UsersClient(
+  `localhost:${port}`,
+  credentials.createInsecure(),
+);
+
+export const noop = () => {};
